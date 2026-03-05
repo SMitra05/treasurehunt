ff5e206bba312896452b936c94f4f33b0e8919a6
Subhadeb Mitra <subhadebmitra@gmail.com>
2026-03-05 23:34:59 +0530

    Remove head section and font link from layout
    Removed the head section and associated font link from layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,20 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-
-        {/* Sci-fi font used in Tectrix website */}
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-
-      </head>
-
       <body>
-
-        {/* Main page content */}
 
         {children}
 
